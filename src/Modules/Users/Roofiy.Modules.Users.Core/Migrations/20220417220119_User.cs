de4a2348347d6486fbc2bc229060ba6b62818420
otopagac <ozgurtopagac@gmail.com>
2022-04-18 01:02:20 +0300

    Migration added.

---
+++ src/Modules/Users/Roofiy.Modules.Users.Core/Migrations/20220417220119_User.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Roofiy.Modules.Users.Core.Migrations
+{
+    public partial class User : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
